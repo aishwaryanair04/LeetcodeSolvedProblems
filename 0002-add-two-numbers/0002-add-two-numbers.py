49598220e38f7a2a58053430f3dee849f4dd90e8
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-01-20 16:54:55 -0600

    Time: 55 ms (80.71%), Space: 16.7 MB (53.06%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,70 +6,69 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-#       MY SOLUTION:
-        curr = l1
-        x = 1
-        num1 = 0
-        while curr:
-            val = curr.val * x
-            num1 += val
-            x = x*10
-            curr = curr.next
-        
-        curr = l2
-        x = 1
-        num2 = 0
-        while curr:
-            val = curr.val * x
-            num2 += val
-            x = x*10
-            curr = curr.next
-        
-        result = str(num1 + num2)
-        list1 = []
-        for char in result:
-            list1.append(int(char))
-        
-        
-        dummy = ListNode(0)
-        tail = dummy
-        
-        for num in list1:
-            new_node = ListNode(num)
-            tail.next = new_node
-            tail = tail.next
-        
-        
-        prev, curr = None, dummy.next
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        
-        return prev
-    
-#     NEETCODE SOLUTION:
-
-
-        dummy = ListNode(0)
+        dummy = ListNode()
         tail = dummy
         carry = 0
-        while l1 and l2:
-            add = l1.val + l2.val + carry
-            if add < 10:
-                new_node = ListNode(add)
-                tail.next = new_node
-            else:
-                mod = add % 10
-                quo = add // 10
-                new_node = ListNode(mod)
-                carry = quo
-                tail.next = new_node
-
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            add = v1 + v2 + carry
+            mod = add % 10
+            quo = add // 10
+            new_node = ListNode(add % 10)
+            carry = add // 10
+            tail.next = new_node
             tail = tail.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         return dummy.next
+        
+#       MY SOLUTION:
+#         curr = l1
+#         x = 1
+#         num1 = 0
+#         while curr:
+#             val = curr.val * x
+#             num1 += val
+#             x = x*10
+#             curr = curr.next
+        
+#         curr = l2
+#         x = 1
+#         num2 = 0
+#         while curr:
+#             val = curr.val * x
+#             num2 += val
+#             x = x*10
+#             curr = curr.next
+        
+#         result = str(num1 + num2)
+#         list1 = []
+#         for char in result:
+#             list1.append(int(char))
+        
+        
+#         dummy = ListNode(0)
+#         tail = dummy
+        
+#         for num in list1:
+#             new_node = ListNode(num)
+#             tail.next = new_node
+#             tail = tail.next
+        
+        
+#         prev, curr = None, dummy.next
+#         while curr:
+#             temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = temp
+        
+#         return prev
+
+
+        
         
         
         
