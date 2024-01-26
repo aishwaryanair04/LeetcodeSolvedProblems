7c107c18f027aaf0d7dc2d022273d6db88a01c79
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-01-25 20:30:39 -0600

    Time: 71 ms (77.38%), Space: 20.1 MB (68.50%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -17,6 +17,23 @@ class NumArray:
             return self.prefix[right] - self.prefix[left-1]
         else:
             return self.prefix[right]
+        
+        
+        # My Solution
+        
+#         def __init__(self, nums: List[int]):
+#         self.prefixArray = []
+#         curr = 0
+#         for val in nums:
+#             curr += val
+#             self.prefixArray.append(curr)
+            
+#         def sumRange(self, left: int, right: int) -> int:
+#         if left > 0:
+#             val = self.prefixArray[right] - self.prefixArray[left-1]
+#             return val
+#         else:
+#             return self.prefixArray[right]
             
         
 
