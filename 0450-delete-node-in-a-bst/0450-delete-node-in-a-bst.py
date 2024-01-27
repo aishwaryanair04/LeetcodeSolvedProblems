cabf097a22d5ac2d5bd91dad44994c8906c92ba4
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-01-27 13:31:57 -0600

    Time: 45 ms (98.75%), Space: 19.8 MB (96.68%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,26 +8,88 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         
         if not root:
-            return None
+            return
         
-        if key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        elif key < root.val:
+        if key < root.val:
             root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
         else:
             if not root.left and not root.right:
                 return None
-            elif not root.left and root.right:
-                return root.right
             elif root.left and not root.right:
                 return root.left
-            else:
-                pnt = root.right
-                while pnt.left:
-                    pnt = pnt.left
-                root.val = pnt.val
-                root.right = self.deleteNode(root.right, pnt.val)
-            
+            elif not root.left and root.right:
+                return root.right
+            elif root.left and root.right:
+                temp = root.right
+                while temp.left:
+                    temp = temp.left
+                root.val = temp.val
+                root.right = self.deleteNode(root.right, temp.val)
+        
         return root
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not root:
+#             return None
+        
+#         if key > root.val:
+#             root.right = self.deleteNode(root.right, key)
+#         elif key < root.val:
+#             root.left = self.deleteNode(root.left, key)
+#         else:
+#             if not root.left and not root.right:
+#                 return None
+#             elif not root.left and root.right:
+#                 return root.right
+#             elif root.left and not root.right:
+#                 return root.left
+#             else:
+#                 pnt = root.right
+#                 while pnt.left:
+#                     pnt = pnt.left
+#                 root.val = pnt.val
+#                 root.right = self.deleteNode(root.right, pnt.val)
+            
+#         return root
                 
                 
