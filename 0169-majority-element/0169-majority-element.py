64301cbaa28f7842034db6232f56c66041eec11c
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-02-11 22:32:51 -0600

    Time: 149 ms (22.54%), Space: 18 MB (83.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        setNums = set(nums)
-        hashmap = {}
+        count = {}
+        res, maxCount = 0,0
         
-        for num in setNums:
-            hashmap[num] = nums.count(num)
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            res = n if count[n] > maxCount else res
+            maxCount = max(maxCount, count[n])
         
-        return  max(hashmap, key=lambda k: hashmap[k])
+        return res
+        
+#         setNums = set(nums)
+#         hashmap = {}
+        
+#         for num in setNums:
+#             hashmap[num] = nums.count(num)
+        
+#         return  max(hashmap, key=lambda k: hashmap[k])
             
         
