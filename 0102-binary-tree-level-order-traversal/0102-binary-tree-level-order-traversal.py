072f0f95529b56f5cc623c937245381f07ee353d
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-02-29 17:27:01 -0600

    Time: 49 ms (10.60%), Space: 17.1 MB (95.17%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,26 +7,79 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        queue = collections.deque()
-        res = []
-        if root:
-            queue.append(root)
+        if not root:
+            return []
         
+        q = collections.deque([root])
+        stack = []
         
-        while queue:
+        while q:
             val = []
-            for i in range(len(queue)):
-                node = queue.popleft()
-                print(node.val)
+            for i in range(len(q)):
+                node = q.popleft()
                 val.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    q.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-            res.append(val)
+                    q.append(node.right)
+            
+            stack.append(val)
+        return stack
+                
+            
+                
+            
+            
+            
         
         
-        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         queue = collections.deque()
+#         res = []
+#         if root:
+#             queue.append(root)
+        
+        
+#         while queue:
+#             val = []
+#             for i in range(len(queue)):
+#                 node = queue.popleft()
+#                 print(node.val)
+#                 val.append(node.val)
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             res.append(val)
+        
+        
+#         return res
         
         
         
