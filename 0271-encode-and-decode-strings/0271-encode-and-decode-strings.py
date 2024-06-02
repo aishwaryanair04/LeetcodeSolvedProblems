64fbe0500b7f7b6b38e3ac87505c3bf52f8c142d
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-06-02 12:46:10 -0500

    Time: 80 ms (20.72%), Space: 16.7 MB (94.47%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,64 +2,6 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-
-        self.hashmap = {}    
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            self.hashmap[sorted_word] = word
-        encoded_words = self.hashmap.keys()
-        s = ""
-        breaker = "^&@"
-        for word in encoded_words:
-            s = s + word + breaker
-
-        return s
-        
-    
-    def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        res = []
-        encoded_words = s.split("^&@")
-
-        for word in encoded_words:
-            if word:
-                res.append(self.hashmap[word])
-
-        return res
-
-                    
-                
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Codec:
-    def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
         encoded_string = ""
         for word in strs:
             encoded_string = encoded_string + str(len(word)) +'#' + word
