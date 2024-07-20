ee4011f6e907b67e66dccc1f070a3a7c293cb603
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-07-20 14:49:22 -0500

    Time: 25 ms (96.75%), Space: 16.5 MB (43.63%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -0,0 +1,37 @@
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        
+         return int(math.sqrt(n))
+        
+#         if n == 0:
+#             return 0
+        
+#         self.bulbs = [0] * n
+#         switch_round = 1
+#         while switch_round <= n:
+#             for i in range(n):
+#                 if (i+1) % switch_round == 0:
+#                     if self.bulbs[i] == 0:
+#                         self.bulbs[i] = 1
+#                     else:
+#                         self.bulbs[i] = 0
+            
+            
+#             switch_round += 1
+
+#         return self.bulbs.count(1)
+                    
+                
+                
+
+                
+                        
+                
+        
+        
+                
+            
+            
+    
+    
+        
