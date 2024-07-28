31ce64dacaaa9f19f2c2c84dccb7cec9e571cba3
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-07-28 16:00:07 -0500

    Time: 28 ms (92.22%), Space: 16.4 MB (63.41%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.py
+++ 0824-goat-latin/0824-goat-latin.py
@@ -1,15 +1,17 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
-        
-        words = sentence.split(' ')
-        
+
+        words = sentence.split(" ")
         for i in range(len(words)):
-            if words[i][0] in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
-                words[i] = words[i] + 'ma' + ('a'*(i+1))
+            if words[i][0].lower() in ('a','e','i','o','u'):
+                words[i] = words[i] + "ma" + ('a' * (i + 1))
+            
             else:
-                letter = words[i][0]
-                words[i] = words[i][1:] + letter + 'ma' + ('a'*(i+1))
+                words[i] = words[i][1:] + words[i][0] + "ma"  +('a' * (i + 1))
         
-        return ' '.join(words)
+
+        return " ".join(words)
+                
+                
         
         
