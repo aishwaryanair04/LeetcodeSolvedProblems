4a1b2855978dc216c541dfb7f1f2006f02834671
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-08-15 16:56:24 -0500

    Time: 174 ms (74.27%), Space: 19.1 MB (8.25%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -0,0 +1,54 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        
+        hashmap = {}
+        
+        for i,n in enumerate(nums):
+            if n in hashmap:
+                hashmap[n].append(i)
+            else:
+                hashmap[n] = [i]
+        
+        degree = max(len(val) for val in hashmap.values())
+        
+        res = len(nums)
+        
+        for indices in hashmap.values():
+            if len(indices) == degree:
+                res = min(res, indices[-1]-indices[0])
+        
+        return res+1
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(nums) == 1:
+#             return 1
+#         degree_nums = self.degree(nums)
+#         print(degree_nums)
+#         res = float("inf")
+        
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if self.degree(nums[i:j+1]) == degree_nums:
+#                     res = min(res, len(nums[i:j+1]))
+#         return res
+                    
+        
+        
+#     def degree(self, ls):
+#         hashmap = {}
+#         for num in set(ls):
+#             hashmap[num] = ls.count(num)
+
+#         return max(hashmap.values())
+        
+            
+        
+        
+            
+        
