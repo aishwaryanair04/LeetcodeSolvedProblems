9ae5d2dd89fd982aafe76733f323f28796eb2aa9
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-08-15 14:17:17 -0500

    Time: 39 ms (33.03%), Space: 16.3 MB (86.52%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.py
@@ -0,0 +1,13 @@
+class Solution:
+    def distinctIntegers(self, n: int) -> int:
+        
+        return n if n<=1 else n-1
+                        
+                
+            
+                
+            
+            
+            
+            
+        
