98f42e930b7ff0932c5c5430ce83aff3280bcf7a
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-09-11 16:51:05 -0500

    Time: 33 ms (79.26%), Space: 16.5 MB (58.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,16 +1,31 @@
 class Solution:
     def fib(self, n: int) -> int:
         
-        def dp(n, arr):
-            if n <= 1:
-                return n
-            if arr[n] != -1:
-                return arr[n]
-            arr[n] = dp(n-1, arr) + dp(n-2, arr)
-            return arr[n]
+        if n <= 1:
+            return n
         
-        arr = [-1] * (n+1)
-        return dp(n, arr)
+        prev2 = 0
+        prev = 1
+        
+        for i in range(2, n+1):
+
+            curr = prev2 + prev
+            prev2 = prev
+            prev = curr
+        
+        return prev
+            
+        
+#         def dp(n, arr):
+#             if n <= 1:
+#                 return n
+#             if arr[n] != -1:
+#                 return arr[n]
+#             arr[n] = dp(n-1, arr) + dp(n-2, arr)
+#             return arr[n]
+        
+#         arr = [-1] * (n+1)
+#         return dp(n, arr)
         
 #         # BASE CASE
 #         if n <= 1:
