c1054bfb1fc4cb0eed7fec1d2fc2d9752686bdc1
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-09-11 17:33:45 -0500

    Time: 26 ms (95.41%), Space: 16.6 MB (14.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,33 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        one, two = 1, 1
+        def dp(n, arr):
+            if n <= 1:
+                return 1
+            
+            if arr[n] != -1:
+                return arr[n]
+            arr[n] = dp(n-1, arr) + dp(n-2, arr)
+            return arr[n]
         
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
+        arr = [-1] * (n+1)
+        return dp(n, arr)
         
-        return one
+        
+            
+            
+            
+            
+    
+        
+#         one, two = 1, 1
+        
+#         for i in range(n - 1):
+#             temp = one
+#             one = one + two
+#             two = temp
+        
+#         return one
         
 
         
