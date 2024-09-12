ca3dd3e06a0b97459191e9b04ff6a057422221ca
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-09-11 20:55:44 -0500

    Time: 55 ms (45.69%), Space: 16.6 MB (44.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,28 +1,76 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
         res = []
         
-        def dfs(i, subset, currSum):
-#             base case1:
+        def dfs(i, arr, currSum):
             if currSum == target:
-                res.append(subset.copy())
+                res.append(arr.copy())
                 return
-#             base case2:
             if i >= len(candidates) or currSum > target:
                 return
             
-            #Decision to add candidates[i]:
-            subset.append(candidates[i])
-            dfs(i, subset, currSum + candidates[i])
-            
-            
-            #Decision to not add candidates[i]:
-            subset.pop()
-            dfs(i+1, subset, currSum)
+            dfs(i, arr + [candidates[i]], currSum + candidates[i])
+            dfs(i+1, arr, currSum)
         
         dfs(0, [], 0)
         return res
+            
+            
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+        
+#         def dfs(i, subset, currSum):
+# #             base case1:
+#             if currSum == target:
+#                 res.append(subset.copy())
+#                 return
+# #             base case2:
+#             if i >= len(candidates) or currSum > target:
+#                 return
+            
+#             #Decision to add candidates[i]:
+#             subset.append(candidates[i])
+#             dfs(i, subset, currSum + candidates[i])
+            
+            
+#             #Decision to not add candidates[i]:
+#             subset.pop()
+#             dfs(i+1, subset, currSum)
+        
+#         dfs(0, [], 0)
+#         return res
             
             
             
