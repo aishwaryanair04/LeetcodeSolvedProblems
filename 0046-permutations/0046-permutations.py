76b022a6c00acaf63e82137a2f858daf64f0cd4d
Aishwarya Nair <67897071+aishwaryanair04@users.noreply.github.com>
2024-09-12 17:38:28 -0500

    Time: 34 ms (90.64%), Space: 16.7 MB (32.52%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,23 +3,71 @@ class Solution:
         
         res = []
         
-        #base case:
-        if len(nums) == 1:
-            return [nums.copy()]
-        
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
+        def dfs(i):
+            if i >= len(nums):
+                res.append(nums[:])
+                return
             
-            print("perms",perms)
-            for perm in perms:
-                print("perm",perm)
-                perm.append(n)
-                res.append(perm)
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                
+                dfs(i + 1)
+                
+                nums[i], nums[j] = nums[j], nums[i]
             
-            nums.append(n)
-        
+        dfs(0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+        
+#         #base case:
+#         if len(nums) == 1:
+#             return [nums.copy()]
+        
+#         for i in range(len(nums)):
+#             n = nums.pop(0)
+#             perms = self.permute(nums)
+            
+#             print("perms",perms)
+#             for perm in perms:
+#                 print("perm",perm)
+#                 perm.append(n)
+#                 res.append(perm)
+            
+#             nums.append(n)
+        
+#         return res
         
         
             
